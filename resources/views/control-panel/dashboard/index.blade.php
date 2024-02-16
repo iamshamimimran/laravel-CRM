d76c214641c5eb3c68b414782795f9b841c1cbbe
Shamim Imran <shamimimran35@gmail.com>
2024-02-16 13:17:38 +0530

    Config Updated

--- resources/views/control-panel/dashboard/index.blade.php
+++ resources/views/control-panel/dashboard/index.blade.php
@@ -5,8 +5,8 @@
     @foreach ($statistics as $entity => $count)
       <div class="card">
         <div class="card-body">
-            <h4 class="card-title">{{ $count }}</h4>
-            <p class="card-text"><small class="text-muted">{{ $entity }}</small></p>
+            <h3 class="card-title">{{ $count }}</h3>
+            <p class="card-text"><h5 class="text-muted">{{ $entity }}</h5></p>
         </div>
       </div>
     @endforeach
